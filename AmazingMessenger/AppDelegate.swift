2e8dd1fe3102662017a9c056b6ceb7d1a92d6dba
AlexCoder13 <sesasha99@gmail.com>
2025-01-11 00:07:07 +0300

    A.M. - without SB, not working, + methods

--- AmazingMessenger/AppDelegate.swift
+++ AmazingMessenger/AppDelegate.swift
@@ -13,7 +13,30 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        self.window = makeWindow()
+        
         return true
+    }
+    
+    
+    func applicationDidBecomeActive(_: UIApplication) {
+        print("- Application moved from <Not Running/Inactive> to <Active>: <\(#function)>")
+    }
+    
+    func applicationWillResignActive(_: UIApplication) {
+        print("- Application moved from <Active> to <Inactive>: <\(#function)>")
+    }
+    
+    func applicationDidEnterBackground(_: UIApplication) {
+        print("- Application moved from <Inactive> to <Background>: <\(#function)>")
+    }
+    
+    func applicationWillEnterForeground(_: UIApplication) {
+        print("- Application moved from <Background> to <Inactive>: <\(#function)>")
+    }
+    
+    func applicationWillTerminate(_: UIApplication) {
+        print("- Application moved from <Inactive> to <Not Running>: <\(#function)>")
     }
 }
 
