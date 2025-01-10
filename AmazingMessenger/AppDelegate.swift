7d3586ea6bf11fe4dd1aae01000d605be93ebe93
AlexCoder13 <sesasha99@gmail.com>
2025-01-11 00:53:11 +0300

    A.M. - without SB, working

--- AmazingMessenger/AppDelegate.swift
+++ AmazingMessenger/AppDelegate.swift
@@ -11,12 +11,15 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            
+            let window = UIWindow (frame: UIScreen.main.bounds)
+            window.rootViewController = ViewController()
+            window.makeKeyAndVisible()
+            self.window = window
         
-        self.window = makeWindow()
-        
-        return true
-    }
+            return true
+        }
     
     
     func applicationDidBecomeActive(_: UIApplication) {
