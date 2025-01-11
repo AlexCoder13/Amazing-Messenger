d506819bd4e9f95515c31aeab30df2505d4f3f3f
AlexCoder13 <sesasha99@gmail.com>
2025-01-12 00:07:49 +0300

    A.M. - 3/4

--- AmazingMessenger/ViewController.swift
+++ AmazingMessenger/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func loadView() {
+        super.loadView()
+        print("- - Called method: \(#function)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("- - Called method: \(#function)")
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("- - Called method: \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("- - Called method: \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("- - Called method: \(#function)")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("- - Called method: \(#function)")
+    }
 }
 
